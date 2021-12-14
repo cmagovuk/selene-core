5d64d0c4341ede702481d673a5a1e2658f103cf7
AlexBates <>
2021-12-14 15:02:27 +0000

    crawler screenshot tidy up

--- selene/core/selenium/crawler.py
+++ selene/core/selenium/crawler.py
@@ -1,29 +1,9 @@
-# %%
 from selene.core.crawler import Crawler
-from selene.core.selenium.tasks import task_screenshot_to_notebook
 
 
-# %%
 class CrawlerSelene(Crawler):
     """
     A crawler class to assist any workflow which requires selenium webdriver.
     
     Inherits selene.core.crawler.Crawler
     """
-
-    def screenshot_to_notebook(self, driver, debug=None):
-        """
-        Display a thumbnail-sized screenshot to a Jupyter notebook,
-        only if the crawler is in debug mode.
-        
-        Parameters
-        ----------
-            driver : selenium.webdriver
-                the initialised webdriver instance
-            debug : bool
-                whether or not the craler is in debug mode
-        """
-        if debug is None:
-            debug = self.debug
-        if debug:
-            task_screenshot_to_notebook(driver, width=600, height=400, logger=None)
