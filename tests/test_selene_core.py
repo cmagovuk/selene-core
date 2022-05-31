a13a115e36e17da818f3289bb32fbe8b8812faa4
AlexGill <>
2022-05-31 10:50:45 +0000

    tidy, fix

--- tests/test_selene_core.py
+++ tests/test_selene_core.py
@@ -16,34 +16,28 @@ from selene.core.utils import *
 
 from selene.core.selenium.driver import *
 
-
 def test_crawler_init():
     assert Crawler() is not None
-    
-    
+       
 def test_crawler_logging():
     crawler = Crawler()
     assert crawler.log('test') is None
-    
     
 def test_element_init():
     el = "test_element"
     logger = get_logger()
     element = Element(el, logger)
     assert element.element == "test_element"
-    
-    
+        
 def test_element_logging():
     el = "test_element"
     element = Element(el, logger = get_logger())
     assert element.log('test') is None
-    
-    
+       
 def test_logger_init():
     logger = get_logger()
     assert logger is not None
-    
-    
+       
 def test_logger_to_file():
     logger = get_logger(to_file=True, 
                         dirpath=".",
@@ -53,13 +47,11 @@ def test_logger_to_file():
     crawler.log('test')
     assert os.path.isfile("./test_logger.log")
     
-
 def test_page_init():
     test_url = "https://www.scrapethissite.com/"
     page = Page(url=test_url, logger = get_logger())
     assert page is not None
     
-
 def test_page_logging():
     test_url = "https://www.scrapethissite.com/"
     logger = get_logger(to_file=True, 
@@ -74,20 +66,17 @@ def test_page_logging():
 def test_utils_get_domain():
     url = "https://www.scrapethissite.com/"
     assert get_domain(url) == "www.scrapethissite.com"
-    
-    
+      
 def test_random_wait():
     @random_wait(seconds_min=2, seconds_max=3)
     def test_func(x):
         print(x)   
     start_time = time.time()  
-    test_func("test")
+    test_func(page)
     end_time = time.time()
     time_elapsed = end_time - start_time
     assert time_elapsed >= 2
       
-
 def test_validate_url():
     url = "https://www.scrapethissite.com/"
     assert validateUrl(url) is True
-
