55cfe12ebc81f8b7466daaac5216f1e9499db518
Alex <85504577+alexgillcma@users.noreply.github.com>
2022-06-07 13:34:42 +0100

    remove unnecessary test reqs

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
     version=__version__,
     packages=find_packages(),
     install_requires=REQUIREMENTS,
-    tests_require=REQUIREMENTS + ["pytest", "pytest-runner"],
     extras_require={
         "tests": REQUIREMENTS_TEST
     },
