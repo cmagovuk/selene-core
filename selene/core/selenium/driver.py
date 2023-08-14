8475dbc4df9108c951612e9ad10172a00cef44d8
AlexGill <Alex.Gill@cma.gov.uk>
2023-08-14 14:59:26 +0000

    black

--- selene/core/selenium/driver.py
+++ selene/core/selenium/driver.py
@@ -59,7 +59,7 @@ def get_driver(
         options.add_argument(f"--user-agent={user_agent}")
 
     # enable browser logging
-    options.set_capability ("goog:loggingPrefs", {"browser": "ALL"})
+    options.set_capability("goog:loggingPrefs", {"browser": "ALL"})
 
     driver = webdriver.Chrome(options=options)
     driver.set_window_rect(x=0, y=0, width=width, height=height)
